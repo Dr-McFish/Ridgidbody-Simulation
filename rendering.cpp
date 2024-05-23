4b792ae986e8edc80a5e8e5290375ee61b8f931c
nc <drmcfish@localhost.localdomain>
2024-05-23 23:32:20 +0200

    Square Mesh

--- rendering.cpp
+++ rendering.cpp
@@ -30,6 +30,25 @@ struct rendering::mesh rendering::create_cube(std::string name) {
 	return cube;
 }
 
+struct rendering::mesh rendering::create_square(std::string name) {
+	struct rendering::mesh square;
+
+	square.scale = 0.5;
+	square.vertices = Eigen::MatrixXf(4, 3);
+	square.vertices <<  1.0,  0.0,  1.0,  
+					   -1.0,  0.0,  1.0,
+					   -1.0,  0.0, -1.0,
+					    1.0,  0.0, -1.0, 
+
+	square.faces = Eigen::MatrixXf(1, 4);
+	square.faces << 1, 2, 3, 4;
+
+	square.mesh = polyscope::registerSurfaceMesh(name, square.vertices, square.faces);
+	square.mesh->setSurfaceColor(glm::vec3(0.1, 0.5, 0.6));
+	return square;
+}
+
+
 void rendering::update_mesh(rendering::mesh& mesh, Eigen::Vector3f position, Eigen::Quaternionf rotaion) {
 Eigen::MatrixXf new_vertices(8, 3);
 		for(int i = 0; i < 8; i++) {
